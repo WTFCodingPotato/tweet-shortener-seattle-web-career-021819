772abb0ddf79c54851b3c044734d76417c0eda93
Joshua M Loran <joshua.loran@gmail.com>
2019-01-24 23:58:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ end
 
 def word_substituter(tweet)
   tweet_array = tweet.split(" ")
-  tweet_array.each_with_index do |word, index|
+  tweet.split(" ").each_with_index do |word, index|
     if dictionary.keys.include?(word.downcase)
       tweet_array[index] = dictionary[word.downcase]
     end
