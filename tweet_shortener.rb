8dfe15b3ecd35e0a51293743d9a39af47e75bbc5
Joshua M Loran <joshua.loran@gmail.com>
2019-01-25 00:28:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,9 +39,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
-    if word_substituter(tweet).length > 140
       word_substituter(tweet)[0..139]
-    end
   else
     tweet
   end
